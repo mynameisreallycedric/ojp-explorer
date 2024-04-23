adc43ea2c62e2d7be701cd0831cd84596f2d4034
Cédric Wagner <cedric.wagner@students.fhnw.ch>
2024-04-23 18:29:46 +0200

    removed unecessary comments and console log

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,10 @@ import useStopEvent from "@/compopsables/services/stopEvent";
 import {onMounted, type Ref, ref} from "vue";
 import type {StopEvent} from "@/types/StopEvent";
 
-const stopEvents = ref<StopEvent | undefined>(undefined);
+const stopEvents = ref<StopEvent>();
 
 onMounted(async () => {
-  // Fetch the stop event and assign the result to stopEvents
   stopEvents.value = await useStopEvent().getStopEventForLocation(8502001);
-
-  // Now stopEvents.value will be updated with the fetched data
-  console.log(stopEvents.value);
 })
 
 console.log(stopEvents)
