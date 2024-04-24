c9f294ca5fe793ab931cea1316ca3459a3d57643
cedric <cedric.wagner@students.fhnw.ch>
2024-04-24 22:31:19 +0200

    installed tailwind and set date for departure to locale time string

---
+++ src/components/Demo/TimeTable/DemoTimeTableInput.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import type {Connection} from "@/types/Connection";
+
+interface Props {
+  connection: Connection
+}
+
+const props = defineProps<Props>();
+
+
+</script>
+
+<template>
+  <div class="flex flex-row border-solid justify-between timetable_connection">
+    <p>{{ connection.info.lineName }}</p>
+    <p>{{ new Date(connection.thisStop.departureTime).toLocaleTimeString() }}</p>
+    <p>{{ connection.info.destination }}</p>
+    <p>{{ connection.info.origin }}</p>
+    <p>{{ connection.thisStop.quay }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+.timetable_connection {
+  width: 600px;
+  margin: 2rem;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 5px ;
+}
+
+</style>
--- src/views/Demo/TimeTableView.vue
+++ src/views/Demo/TimeTableView.vue
@@ -3,6 +3,7 @@ import useStopEvent from "@/compopsables/services/stopEvent";
 import {onMounted, type Ref, ref} from "vue";
 import type {StopEvent} from "@/types/StopEvent";
 import type {Connection} from "@/types/Connection";
+import DemoTimeTableInput from "@/components/Demo/TimeTable/DemoTimeTableInput.vue";
 
 const stopEvents = ref<StopEvent>();
 
@@ -13,10 +14,7 @@ onMounted(async () => {
 
 <template>
   <div v-for="connection in stopEvents?.connections as Connection[]">
-    <h1>{{ connection.info.day }}</h1>
-    <p>{{ connection.info.destination }}</p>
-    <p>{{ connection.info.origin }}</p>
-    <p>{{ connection.info.connectionType }}</p>
+    <DemoTimeTableInput :connection="connection" />
   </div>
 </template>
 
