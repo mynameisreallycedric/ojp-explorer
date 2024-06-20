9b19ad394b074f0beaae2de14d66ba60927cfa07
Cédric Wagner <cedric.wagner@students.fhnw.ch>
2024-06-20 19:56:54 +0200

    working on nav bar

--- src/components/Main/Layout/MainNavBar.vue
+++ src/components/Main/Layout/MainNavBar.vue
@@ -9,7 +9,7 @@ const swaggerUrl = computed<string>(() => {
 </script>
 
 <template>
-  <nav class="flex w-full justify-between my-2">
+  <nav class="flex w-full justify-between my-2 main-nav">
     <RouterLink to="/"><h3>OJP Explorer 🚂</h3></RouterLink>
     <div class="inline-flex flex-row items-baseline gap-4">
       <a :href="swaggerUrl" target="_blank">Go To Swagger</a>
@@ -19,5 +19,10 @@ const swaggerUrl = computed<string>(() => {
 </template>
 
 <style scoped lang="scss">
+@import "src/assets/scss/variables";
 
+.main-nav {
+  padding: 0.5rem 0 0.5rem 0;
+  border-bottom: 2px solid $pt-main-light-gray;
+}
 </style>
